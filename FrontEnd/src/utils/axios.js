f106fc4706524845818a975f77f34405f90b10a4
ShubhamBisht18 <155867546+ShubhamBisht18@users.noreply.github.com>
2025-09-14 22:57:35 +0530

    deployment setups

--- FrontEnd/src/utils/axios.js
+++ FrontEnd/src/utils/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const instance  = axios.create({
-    // baseURL: 'https://vastrfy.onrender.com/api',
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://vastrfy.onrender.com/api',
+    // baseURL: 'http://localhost:5000/api',
     withCredentials: true
 })
 
